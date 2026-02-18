4c84bcbd2aec84f6c7102213bb01bc613cefa2c0
Mobin Mardane <Mobin1386mardane@gmail.com>
2026-02-18 20:46:45 +0330

    Final commit

--- Project.py
+++ Project.py
@@ -1,1 +1,18 @@
-import 
+import csv 
+import requests
+request = requests.get("https://openlibrary.org/search.json?q=python")
+response = request.json()
+
+f = open("book.csv" , "w")
+data = csv.writer(f)
+header = data.writerow(["Book" , "Year" , "Author"])
+
+for book in response["docs"]:
+	title = book.get("title")
+	year = book.get("first_publish_year")
+	author = book.get("author")
+	try:
+		if (int(year) > 2000):
+			data.writerow([title , year , author])
+	except:
+		pass
